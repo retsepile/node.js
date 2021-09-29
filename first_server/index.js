9fe05fb6065d27d2b68b2af40f65d7e0327ab11b
retsepile <Retshepilekoloko27@gmail.com>
2021-09-29 09:20:13 +0200

    second commit

--- first_server/index.js
+++ first_server/index.js
@@ -4,9 +4,22 @@ const hostname = 'localhost';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.end('Welcome to Node');
-});
+    const { url } = req; 
 
+    console.log(url);
+
+    if (url === '/translations'){
+        const translations = {1:'one', 2:'two', 3:'three'}
+
+       res.setHeader('Content-Type','application/json');
+
+       res.write(JSON.stringify(translations));
+
+       res.end();
+    }
+
+    res.end('Welcome to Node!!!');
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at ${hostname}: ${port}`)
